156bd6aeff3eeded96a3ce0b6c6f0f3dca1679dc
Ashish <ashish@gmail.com>
2025-09-12 17:02:42 +0530

    refactor: comment out JpaConfig class and its related annotations

--- src/main/java/com/ashishtech/admin/config/JpaConfig.java
+++ src/main/java/com/ashishtech/admin/config/JpaConfig.java
@@ -1,14 +1,14 @@
-package com.ashishtech.admin.config;
+// package com.ashishtech.admin.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+// import org.springframework.boot.autoconfigure.domain.EntityScan;
+// import org.springframework.context.annotation.Configuration;
 // import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
-@ConditionalOnProperty(name = "spring.datasource.url")
+// @Configuration
+// @ConditionalOnProperty(name = "spring.datasource.url")
 // @EnableJpaRepositories(basePackages = "com.ashishtech.admin.repository")
-@EntityScan(basePackages = "com.ashishtech.admin.entity")
+// @EntityScan(basePackages = "com.ashishtech.admin.entity")
 // public class JpaConfig {
     // JPA and DataSource beans will only be loaded if spring.datasource.url is set
 // }
